09e22b2b6be9abbbae0073e20709b6bd11d0952a
versx <versefx@gmail.com>
2018-12-12 07:18:24 -0800

    Fix list element comparison

--- src/Extensions/GenericsExtensions.cs
+++ src/Extensions/GenericsExtensions.cs
@@ -72,7 +72,7 @@
 
             for (int i = 0; i < list1.Count; i++)
             {
-                if (list1[i].Equals(list2[i]))
+                if (!EqualityComparer<T>.Default.Equals(list1[i], list2[i]))
                     return false;
             }
 
