cbf3a782151e95207ca53708c3d851099c3f2cfa
versx <versx@ver.sx>
2020-11-06 07:38:13 -0800

    Fix nest list embed description length check

--- src/Commands/Nests.cs
+++ src/Commands/Nests.cs
@@ -99,8 +99,9 @@
                         var gmapsLink = string.Format(Strings.GoogleMaps, nest.Latitude, nest.Longitude);
                         // TODO: Check if possible shiny
                         eb.Description += $"[**{nest.Name}**]({gmapsLink}): {pkmnName} (#{nest.PokemonId}) {nest.Average:N0} per hour\r\n";
-                        if (eb.Description.Length >= 2300)
+                        if (eb.Description.Length >= 2048)
                         {
+                            var message = eb.Description.Substring(0, Math.Min(eb.Description.Length, 2048));
                             await channel.SendMessageAsync(embed: eb);
                             eb = new DiscordEmbedBuilder
                             {
