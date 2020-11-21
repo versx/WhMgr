57276d2a2ae6eebb00ac6c7e14cb264a4e654c37
versx <versx@ver.sx>
2020-11-20 18:26:05 -0800

    Allow Pokemon/PvP specific city subscriptions (#35)
    * Enable subscriptions for Pokemon/PvP for specific cities
    
    * Automatically disable user subscriptions if not a supporter/donor
    
    * Remove role check for PvP subs
    
    * oops lol
    
    * Ignore subscriptions without city set
    
    * Combine subscription entries instead of separate per city
    
    * Ignore geofence if distance is set
    
    * Add distance location link to subscriptions info
    
    * Simplify url shortener and address lookup
    
    * Fix yourls, add missing `timestamp` param to query
    
    * Fix areas in subscriptions info command
    
    * Add custom user subscription limits per type
    
    * fixes
    
    * Fix database migrator to only wait 30 seconds once
    
    * Fix typo
    
    * format distance
    
    * Update help description to make min/max level and IV and IV lists more noticable.
    
    * Change distance/geofence check to OR (if either match)
    
    * Add ascii art, version, and dotnet runtime version to logs
    
    * project version bump
    
    * Fix case sensitivity issue with areass check
    
    * Possibly fix negative time remaining for raids, lures, and invasions in different timezones
    
    * Update docs

--- src/Data/Subscriptions/Models/QuestSubscription.cs
+++ src/Data/Subscriptions/Models/QuestSubscription.cs
@@ -1,8 +1,10 @@
 ﻿namespace WhMgr.Data.Subscriptions.Models
 {
-    using ServiceStack.DataAnnotations;
+    using System;
+    using System.Collections.Generic;
 
     using Newtonsoft.Json;
+    using ServiceStack.DataAnnotations;
 
     [
         JsonObject("quests"),
@@ -28,6 +30,6 @@
             Alias("city"), 
             Required
         ]
-        public string City { get; set; }
+        public List<string> Areas { get; set; }
     }
 }
