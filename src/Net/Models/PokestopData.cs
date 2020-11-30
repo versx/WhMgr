7ca51ef93092c3deb8f6af64d6c0236009a8fc84
versx <versx@ver.sx>
2020-11-29 20:22:34 -0800

    Fix issue with parser for Pokestops

--- src/Net/Models/PokestopData.cs
+++ src/Net/Models/PokestopData.cs
@@ -115,11 +115,11 @@
             var properties = GetProperties(client.Guilds[guildId], whConfig, city);
             var eb = new DiscordEmbedBuilder
             {
-                Title = DynamicReplacementEngine.ReplaceText(alert.Title, properties),
-                Url = DynamicReplacementEngine.ReplaceText(alert.Url, properties),
-                ImageUrl = DynamicReplacementEngine.ReplaceText(alert.ImageUrl, properties),
-                ThumbnailUrl = DynamicReplacementEngine.ReplaceText(alert.IconUrl, properties),
-                Description = DynamicReplacementEngine.ReplaceText(alert.Content, properties),
+                Title = Renderer.Parse(alert.Title, properties),
+                Url = Renderer.Parse(alert.Url, properties),
+                ImageUrl = Renderer.Parse(alert.ImageUrl, properties),
+                ThumbnailUrl = Renderer.Parse(alert.IconUrl, properties),
+                Description = Renderer.Parse(alert.Content, properties),
                 Color = HasInvasion
                     ? new DiscordColor(server.DiscordEmbedColors.Pokestops.Invasions)
                     : HasLure
