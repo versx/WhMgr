74371051c52823e2480ee81e58e82bedb918a740
Philippe <m_philippe@rocketmail.com>
2020-12-04 17:04:25 +0100

    Update update.sh to allow parameters (#84)
    update.sh examples
    will copy examples to build folder
    
    update.sh geofences
    will copy geofences to build folder
    
    update.sh all
    will copy examples and geofences to build folder
    
    Co-authored-by: pizzamann <pizzamann113@gmail.com>

--- update.sh
+++ update.sh
@@ -1,23 +1,33 @@
-# Pull latest Git repository
-echo "Pulling latest Git repository changes..."
-git pull
+#!/usr/bin/env bash
 
-# Build WhMgr.dll
+# Git remote update && git pull
+echo "Get newest changes from Repo"
+git remote update && git pull
+
+# Build WhMgrr.dll
 echo "Building WhMgr..."
-~/.dotnet/dotnet build
+~/.dotnet/dotnet build || dotnet build
 
-# Copy example config
-#echo "Copying example files..."
-#cp -R examples/alerts bin/debug/netcoreapp2.1/alerts/
-#cp -R examples/filters bin/debug/netcoreapp2.1/filters/
-#cp -R examples/templates bin/debug/netcoreapp2.1/templates/
+# Copy example configs
+if [ "$1" == "examples" ] || [ "$1" == "example" ] || [ "$1" == "all" ]; then
+    echo "Copying examples"
+    cp -R "examples/alerts" "bin/debug/netcoreapp2.1/"
+    cp -R "examples/filters" "bin/debug/netcoreapp2.1/"
+    cp -R "examples/templates" "bin/debug/netcoreapp2.1/"
+fi
+if [ "$1" == "geofences" ] || [ "$1" == "geofence" ] || [ "$1" == "all" ]; then
+    echo "Copying geofences..."
+    cp -R "geofences" "bin/debug/netcoreapp2.1/"
+fi
 
-# Copy locale translation files
-echo "Copying locale translation files..."
-cp -R static/locale bin/debug/netcoreapp2.1/static/locale
+# Copy localte translation files
+echo "Copying locale translation files... "
+mkdir -p "bin/debug/netcoreapp2.1/static"
+cp -R "static/locale" "bin/debug/netcoreapp2.1/static/"
 
 echo "Copying latest master file..."
-cp static/data/masterfile.json bin/debug/netcoreapp2.1/static/data/masterfile.json
-cp static/data/cpMultipliers.json bin/debug/netcoreapp2.1/static/data/cpMultipliers.json
+mkdir -p "bin/debug/netcoreapp2.1/static/data"
+cp "static/data/masterfile.json" "bin/debug/netcoreapp2.1/static/data/masterfile.json"
+cp "static/data/cpMultipliers.json" "bin/debug/netcoreapp2.1/static/data/cpMultipliers.json"
 
 echo "Update Complete"
