b593536d028af1ea53ef430abe4c30b7d30a62fb
versx <versx@ver.sx>
2020-12-16 07:47:11 -0800

    oops, remove duplicate PokestopLureType enum

--- src/Net/Models/PokestopData.cs
+++ src/Net/Models/PokestopData.cs
@@ -223,35 +223,4 @@
 
         #endregion
     }
-
-    /// <summary>
-    /// Pokestop lure type
-    /// </summary>
-    public enum PokestopLureType : ushort
-    {
-        /// <summary>
-        /// No Pokestop lure deployed
-        /// </summary>
-        None = 0,
-
-        /// <summary>
-        /// Normal Pokestop lure deployed
-        /// </summary>
-        Normal = 501,
-
-        /// <summary>
-        /// Glacial Pokestop lure deployed
-        /// </summary>
-        Glacial = 502,
-
-        /// <summary>
-        /// Mossy Pokestop lure deployed
-        /// </summary>
-        Mossy = 503,
-
-        /// <summary>
-        /// Magnetic Pokestop lure deployed
-        /// </summary>
-        Magnetic = 504
-    }
 }
