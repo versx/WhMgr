fecdee69e68435c9ccfb172110ff6a42704f5886
thunder123456 <36616001+thunder123456@users.noreply.github.com>
2020-12-22 22:08:43 -0600

    Add Areas command (#108)
    * Add Area command
    
    * Enable areas command if EnableCities is false
    
    * remove unused config option
    
    * change embed color
    
    Co-authored-by: Thunder <thunder@domain.com>

--- src/Bot.cs
+++ src/Bot.cs
@@ -188,6 +188,10 @@
                 {
                     commands.RegisterCommands<Feeds>();
                 }
+                else
+                {
+                    commands.RegisterCommands<Areas>();
+                }
 
                 _logger.Info($"Configured Discord server {guildId}");
                 if (!_servers.ContainsKey(guildId))
