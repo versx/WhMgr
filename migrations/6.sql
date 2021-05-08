278966e0e138c48e0675861be57bbfa9a6ecd312
versx <versx@ver.sx>
2021-05-08 03:06:29 -0700

    Update 6.sql

--- migrations/6.sql
+++ migrations/6.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `invasions`
-ADD COLUMN `pokestop_name` VARCHAR(255);
+ADD COLUMN `pokestop_name` VARCHAR(255) DEFAULT NULL;
 
 ALTER TABLE `invasions`
-ADD COLUMN `grunt_type` TINYINT(2);
+ADD COLUMN `grunt_type` TINYINT(2) DEFAULT NULL;
