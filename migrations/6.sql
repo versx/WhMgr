50028e813b092ddc2746786d518426cdd1f71eff
versx <versx@ver.sx>
2021-05-08 03:08:00 -0700

    Update 6.sql

--- migrations/6.sql
+++ migrations/6.sql
@@ -1,4 +1,7 @@
 ALTER TABLE `invasions`
+MODIFY COLUMN `reward_pokemon_id` SMALLINT(5) DEFAULT NULL;
+
+ALTER TABLE `invasions`
 ADD COLUMN `pokestop_name` VARCHAR(255) DEFAULT NULL;
 
 ALTER TABLE `invasions`
