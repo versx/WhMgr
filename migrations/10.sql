1cdc56198c36e35b6b334bbf89ed812278559e74
versx <versx@ver.sx>
2021-08-24 21:35:10 -0700

    :feelslapras:

--- migrations/10.sql
+++ migrations/10.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `pokemon` 
-MODIFY COLUMN `pokemon_id` text NOT NULL;
-
 ALTER TABLE `pokemon`
 DROP INDEX `ix_pokemon_id`;
+
+ALTER TABLE `pokemon` 
+MODIFY COLUMN `pokemon_id` text NOT NULL;
