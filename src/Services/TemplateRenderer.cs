8377c914cebb4d7bba89b24ee3d91d663f827d96
versx <versx@ver.sx>
2021-09-22 21:49:21 -0700

    Add Handlebar helpers for pokemon name, form, and costume

--- src/Services/TemplateRenderer.cs
+++ src/Services/TemplateRenderer.cs
@@ -1,9 +1,10 @@
 ﻿namespace WhMgr.Services
 {
-    using System;
-
     using HandlebarsDotNet;
     using HandlebarsDotNet.Helpers;
+
+    using WhMgr.Data;
+    using WhMgr.Localization;
 
     public static class TemplateRenderer
     {
@@ -13,9 +14,29 @@
         {
             _context = Handlebars.Create();
             _context.Configuration.TextEncoder = null;
+            // GetPokemonName helper
             _context.RegisterHelper("getPokemonName", new HandlebarsHelper((writer, ctx, args) =>
             {
-                // TODO: GetPokemonName helper
+                if (!uint.TryParse(args[0]?.ToString(), out var pokeId))
+                    return;
+                var pkmnName = MasterFile.GetPokemon(pokeId, 0).Name;
+                writer.Write(pkmnName);
+            }));
+            // GetFormName helper
+            _context.RegisterHelper("getFormName", new HandlebarsHelper((writer, ctx, args) =>
+            {
+                if (!uint.TryParse(args[0].ToString(), out var formId))
+                    return;
+                var formName = Translator.Instance.GetFormName(formId);
+                writer.Write(formName);
+            }));
+            // GetCostumeName helper
+            _context.RegisterHelper("getCostumeName", new HandlebarsHelper((writer, ctx, args) =>
+            {
+                if (!uint.TryParse(args[0].ToString(), out var costumeId))
+                    return;
+                var costumeName = Translator.Instance.GetCostumeName(costumeId);
+                writer.Write(costumeName);
             }));
             // TODO: Add other helpers
             HandlebarsHelpers.Register(_context);
