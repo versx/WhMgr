5f184be5e52964e8fb6717f28e837d717abf74bb
versx <versx@ver.sx>
2021-09-26 18:53:02 -0700

    Add backlog queue for rate limited webhook alarms

--- src/Utilities/NetUtils.cs
+++ src/Utilities/NetUtils.cs
@@ -1,11 +1,31 @@
 ﻿namespace WhMgr.Utilities
 {
     using System;
+    using System.Collections.Generic;
     using System.Net;
     using System.Threading;
 
+    public class WebhookQueueItem
+    {
+        public string Url { get; set; }
+
+        public string Json { get; set; }
+
+        public int RetryAfter { get; set; }
+    }
+
     public static class NetUtils
     {
+        private static readonly Queue<WebhookQueueItem> _backlogQueue = new();
+        private static readonly System.Timers.Timer _timer = new();
+
+        static NetUtils()
+        {
+            _timer.Elapsed += (sender, e) => HandleBacklogQueue();
+            _timer.Interval = 200;
+            _timer.Start();
+        }
+
         /// <summary>
         /// Sends webhook data
         /// </summary>
@@ -17,7 +37,7 @@
             {
                 wc.Headers.Add(HttpRequestHeader.ContentType, "application/json");
                 //wc.Headers.Add(HttpRequestHeader.Authorization, "Bot base64_auth_token");
-                //wc.Headers.Add(HttpRequestHeader.UserAgent, "");
+                wc.Headers.Add(HttpRequestHeader.UserAgent, Strings.BotName);
                 try
                 {
                     var resp = wc.UploadString(webhookUrl, json);
@@ -39,8 +59,14 @@
                             if (!int.TryParse(retryAfter, out var retry))
                                 return;
 
-                            Thread.Sleep(retry);
-                            SendWebhook(webhookUrl, json);
+                            //Thread.Sleep(retry);
+                            //SendWebhook(webhookUrl, json);
+                            _backlogQueue.Enqueue(new WebhookQueueItem
+                            {
+                                Url = webhookUrl,
+                                Json = json,
+                                RetryAfter = retry,
+                            });
                             break;
                         case 400:
                             Console.WriteLine($"Failed to send webhook: {ex}");
@@ -71,6 +97,17 @@
             }
 
             return null;
+        }
+
+        private static void HandleBacklogQueue()
+        {
+            if (_backlogQueue.Count == 0)
+                return;
+
+            var item = _backlogQueue.Dequeue();
+            Thread.Sleep(item.RetryAfter);
+
+            SendWebhook(item.Url, item.Json);
         }
     }
 }
