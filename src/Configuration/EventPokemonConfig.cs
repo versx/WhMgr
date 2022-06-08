d962d1b667aa0b10bf7773060dadad7c5c43a746
versx <versx@ver.sx>
2022-06-08 16:05:19 -0700

    Implement filter type logic for event pokemon

--- src/Configuration/EventPokemonConfig.cs
+++ src/Configuration/EventPokemonConfig.cs
@@ -8,6 +8,9 @@
 
     public class EventPokemonConfig
     {
+        [JsonPropertyName("enabled")]
+        public bool Enabled { get; set; }
+
         /// <summary>
         /// Gets or sets the event Pokemon IDs list
         /// </summary>
@@ -22,9 +25,17 @@
         public int MinimumIV { get; set; } = 90;
 
         /// <summary>
-        /// Gets or sets the event pokemon filter type
+        /// Gets or sets the event pokemon filter type.
+        /// 
+        /// Explaination: Filtering type to use with deemed "event" Pokemon.
+        /// Set to `Exclude` if you do not want the Pokemon reported unless
+        /// it meets the minimumIV value set (or is 0% or has PvP ranks).
+        /// Set to `Include` if you only want the Pokemon reported if it meets
+        /// the minimum IV value set. No other Pokemon will be reported other
+        /// than those in the event list. 
+        ///
         /// </summary>
         [JsonPropertyName("type")]
         public FilterType FilterType { get; set; }
-    }
+}
 }
