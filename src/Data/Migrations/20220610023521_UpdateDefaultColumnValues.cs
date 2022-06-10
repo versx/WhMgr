2237cb44851d9d9b2ff46e203b06bc8a8749af2a
versx <versx@ver.sx>
2022-06-09 20:42:32 -0700

    Fix migration file

--- src/Data/Migrations/20220610023521_UpdateDefaultColumnValues.cs
+++ src/Data/Migrations/20220610023521_UpdateDefaultColumnValues.cs
@@ -10,107 +10,56 @@ namespace WhMgr.Migrations
                 name: "gender",
                 table: "pvp",
                 type: "varchar(1)",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "longtext")
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+                nullable: false);
 
             migrationBuilder.AlterColumn<string>(
                 name: "gender",
                 table: "pokemon",
                 type: "varchar(1)",
                 nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "longtext",
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+                defaultValue: '*');
 
             migrationBuilder.AlterColumn<string>(
                 name: "name",
                 table: "locations",
                 type: "longtext",
                 nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "longtext",
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+                defaultValue: null);
 
             migrationBuilder.AlterColumn<string>(
                 name: "pokestop_name",
                 table: "quests",
                 type: "longtext",
                 nullable: true,
-                defaultValue: null,
-                oldClrType: typeof(string),
-                oldType: "longtext",
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+                defaultValue: null);
 
             migrationBuilder.AlterColumn<string>(
                 name: "pokestop_name",
                 table: "invasions",
                 type: "longtext",
                 nullable: true,
-                defaultValue: null,
-                oldClrType: typeof(string),
-                oldType: "longtext",
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+                defaultValue: null);
 
             migrationBuilder.AlterColumn<string>(
                 name: "pokestop_name",
                 table: "lures",
                 type: "longtext",
                 nullable: true,
-                defaultValue: null,
-                oldClrType: typeof(string),
-                oldType: "longtext",
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+                defaultValue: null);
 
             migrationBuilder.AlterColumn<string>(
                 name: "name",
                 table: "gyms",
                 type: "longtext",
                 nullable: true,
-                defaultValue: null,
-                oldClrType: typeof(string),
-                oldType: "longtext",
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "name",
-                table: "gyms",
-                type: "longtext",
-                nullable: true,
-                defaultValue: null,
-                oldClrType: typeof(string),
-                oldType: "longtext",
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+                defaultValue: null);
 
             migrationBuilder.AlterColumn<int>(
                 name: "max_cp",
                 table: "pokemon",
                 type: "int",
                 nullable: false,
-                defaultValue: int.MaxValue,
-                oldClrType: typeof(int),
-                oldType: "int",
-                oldNullable: false)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+                defaultValue: int.MaxValue);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
