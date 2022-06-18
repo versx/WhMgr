d0a79c95a19af5c03c0c83cd0d5389b7c24b7445
versx <versx@ver.sx>
2022-06-18 00:08:38 -0700

    Add health checks API (#230)
    * Add health checks api for dependent services and resources
    
    * Update docs with healthchecks api example
    
    * Fix formatting

---
+++ src/Diagnostics/BasicHealthCheck.cs
@@ -0,0 +1,40 @@
+﻿namespace WhMgr.Diagnostics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+    /// <summary>
+    /// Basic example health check class if others want to implement their own.
+    /// </summary>
+    public class BasicHealthCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            // Description about health check or if degraded or unhealthy, explain error(s)/issue(s)
+            var description = "Test description about health check, or if degraded or unhealthy - explain errors/issues.";
+
+            // Explanation about error
+            var exception = new Exception("Explaination about error.");
+
+            // Extra key value pair data
+            var data = new Dictionary<string, object>();
+
+            return await Task.FromResult
+            (
+                new HealthCheckResult
+                (
+                    HealthStatus.Healthy,
+                    description,
+                    exception,
+                    data
+                )
+            );
+        }
+    }
+}
