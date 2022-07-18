13e7a7d3c2426739c4fc8e9639e224677de56e1b
versx <versx@ver.sx>
2022-07-17 21:48:52 -0700

    Fix Locale Translations Overwrite (#307)
    * fix issue overwriting locale translations
    
    * fix Nidoran symbols in locales

--- src/Localization/Translator.cs
+++ src/Localization/Translator.cs
@@ -6,6 +6,7 @@
     using System.Linq;
     using System.Threading.Tasks;
 
+    using Microsoft.Extensions.Logging;
     using ActivityType = POGOProtos.Rpc.HoloActivityType;
     using AlignmentType = POGOProtos.Rpc.PokemonDisplayProto.Types.Alignment;
     using CharacterCategory = POGOProtos.Rpc.EnumWrapper.Types.CharacterCategory;
@@ -19,6 +20,9 @@
 
     public class Translator : Language<string, string, Dictionary<string, string>>
     {
+        private static readonly ILogger<Translator> _logger =
+            new Logger<Translator>(LoggerFactory.Create(x => x.AddConsole()));
+
         private const string SourceLocaleUrl = "https://raw.githubusercontent.com/WatWowMap/pogo-translations/master/static/locales/";
         private static readonly string _appLocalesFolder = Directory.GetCurrentDirectory() + $"/../{Strings.LocaleFolder}";
         private static readonly string _binLocalesFolder = Directory.GetCurrentDirectory() + $"/{Strings.BasePath}/{Strings.LocaleFolder}";
@@ -35,57 +39,62 @@
 
         #endregion
 
-        public async Task CreateLocaleFiles()
-        {
-            var files = Directory.GetFiles(_appLocalesFolder, "*.json")
-                                 .Select(fileName => Path.GetFileName(fileName))
-                                 .Where(fileName => fileName.StartsWith('_'))
-                                 .ToList();
+        #region Static Methods
 
+        public static async Task CreateLocaleFilesAsync()
+        {
+            // Copy any missing base locale files to bin directory
+            await CopyLocaleFilesAsync();
+
+            var files = GetBaseLocaleFileNames();
             foreach (var file in files)
             {
-                var locale = Path.GetFileName(file).Replace("_", null);
-                var localeFile = locale;
+                // Replace locale prefix
+                var localeFile = Path.GetFileName(file).Replace("_", null);
+                var locale = Path.GetFileNameWithoutExtension(localeFile);
 
-                var json = await NetUtils.GetAsync(SourceLocaleUrl + locale);
+                var url = SourceLocaleUrl + localeFile;
+                var json = await NetUtils.GetAsync(url);
                 if (json == null)
                 {
-                    Console.WriteLine($"Failed to fetch locales from {SourceLocaleUrl + locale}, skipping...");
+                    _logger.LogWarning($"Failed to fetch locales from {url}, skipping...");
                     return;
                 }
+
+                _logger.LogInformation($"Creating locale {locale}...");
                 var remote = json.FromJson<Dictionary<string, string>>();
-
-                Console.WriteLine($"Creating locale {locale}");
-
-                var keys = remote.Keys.ToList();
-                for (var i = 0; i < keys.Count; i++)
+                foreach (var (key, _) in remote)
                 {
-                    var key = keys[i];
-                    remote[key] = remote[key].Replace("%", "{");
-                    remote[key] = remote[key].Replace("}", "}}");
+                    // Make locale variables compliant with Handlebars/Mustache templating
+                    remote[key] = remote[key].Replace("%", "{")
+                                             .Replace("}", "}}");
                 }
 
                 if (locale != "en")
                 {
                     // Include en as fallback first
-                    var appTransFallback = File.ReadAllText(
+                    var enTransFallback = File.ReadAllText(
                         Path.Combine(_appLocalesFolder, "_en.json")
                     );
-                    var fallbackTranslations = appTransFallback.FromJson<Dictionary<string, string>>();
-                    remote = MergeDictionaries(remote, fallbackTranslations);
+                    var fallbackTranslations = enTransFallback.FromJson<Dictionary<string, string>>();
+                    remote = remote.Merge(fallbackTranslations, updateValues: true);
                 }
 
-                var appTranslations = File.ReadAllText(Path.Combine(_appLocalesFolder, file));
-                remote = MergeDictionaries(remote, appTranslations.FromJson<Dictionary<string, string>>());
+                var appTranslationsData = File.ReadAllText(Path.Combine(_appLocalesFolder, file));
+                var appTranslations = appTranslationsData.FromJson<Dictionary<string, string>>();
+                remote = remote.Merge(appTranslations, updateValues: true);
 
                 File.WriteAllText(
                     Path.Combine(_binLocalesFolder, localeFile),
                     remote.ToJson()
                 );
-                Console.WriteLine($"{localeFile} file saved.");
+                _logger.LogInformation($"{locale} file saved.");
             }
         }
 
+        #endregion
+
+        #region Public Methods
 
         public override string Translate(string value)
         {
@@ -196,19 +205,40 @@
             return Translate($"grunt_{(int)gruntType}");
         }
 
-        private static Dictionary<string, string> MergeDictionaries(Dictionary<string, string> locales1, Dictionary<string, string> locales2)
+        #endregion
+
+        #region Private Methods
+
+        private static async Task CopyLocaleFilesAsync()
         {
-            var result = locales1;
-            foreach (var (key, value) in locales2)
+            // Copy base locale files from app directory to bin directory if they do not exist
+            var files = GetBaseLocaleFileNames();
+            foreach (var file in files)
             {
-                if (result.ContainsKey(key))
-                {
-                    // Key already exists, skip...
+                // Replace locale prefix
+                var localeFile = Path.GetFileName(file);
+                var localeBin = Path.Combine(_binLocalesFolder, localeFile);
+                if (File.Exists(localeBin))
                     continue;
-                }
-                result.Add(key, value);
+
+                _logger.LogDebug($"Copying base locale '{localeFile}' to {localeBin}...");
+                var baseLocalePath = Path.Combine(_appLocalesFolder, file);
+                File.Copy(baseLocalePath, localeBin);
             }
-            return result;
+
+            await Task.CompletedTask;
         }
+
+        private static List<string> GetBaseLocaleFileNames(string extension = "*.json", string prefix = "_")
+        {
+            // Get a list of locale file names that have prefix '_'
+            var files = Directory.GetFiles(_appLocalesFolder, extension)
+                                 .Select(fileName => Path.GetFileName(fileName))
+                                 .Where(fileName => fileName.StartsWith(prefix))
+                                 .ToList();
+            return files;
+        }
+
+        #endregion
     }
 }
